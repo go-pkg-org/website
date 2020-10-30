d59f166aca0a87f6799fdf99ee87e3ccd2c21b42
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-10-30 15:02:14 +0100

    Use Icon components instead of svgs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import { Icon } from '../components';
+
 const Hero = ({
   titleStart = 'Reliable package manager for',
   titleEnd = 'Golang software',
@@ -66,20 +68,7 @@ const Features = () => {
               <div class="flex">
                 <div class="flex-shrink-0">
                   <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-                    <svg
-                      class="h-6 w-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-                      />
-                    </svg>
+                    <Icon.GlobeAltOutline size={24} />
                   </div>
                 </div>
                 <div class="ml-4">
@@ -98,20 +87,7 @@ const Features = () => {
               <div class="flex">
                 <div class="flex-shrink-0">
                   <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-                    <svg
-                      class="h-6 w-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
-                      />
-                    </svg>
+                    <Icon.ScaleOutline size={24} />
                   </div>
                 </div>
                 <div class="ml-4">
@@ -130,20 +106,7 @@ const Features = () => {
               <div class="flex">
                 <div class="flex-shrink-0">
                   <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-                    <svg
-                      class="h-6 w-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M13 10V3L4 14h7v7l9-11h-7z"
-                      />
-                    </svg>
+                    <Icon.LightningBoltOutline size={24} />
                   </div>
                 </div>
                 <div class="ml-4">
@@ -162,20 +125,7 @@ const Features = () => {
               <div class="flex">
                 <div class="flex-shrink-0">
                   <div class="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-                    <svg
-                      class="h-6 w-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                      />
-                    </svg>
+                    <Icon.AnnotationOutline size={24} />
                   </div>
                 </div>
                 <div class="ml-4">
